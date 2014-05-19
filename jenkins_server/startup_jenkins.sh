45123b8fae31a92df2de02769b73e52a111a67cc
vagrant <vagrant@precise64.(none)>
2014-05-19 21:10:41 +0000

    Jenkins configuration update (folder plugins rigths)

--- jenkins_server/startup_jenkins.sh
+++ jenkins_server/startup_jenkins.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-mkdir -p /var/lib/jenkins
-chown jenkins /var/lib/jenkins
-exec su jenkins -c "java -jar /usr/share/jenkins/jenkins.war"
+
+## see also https://updates.jenkins-ci.org/download/plugins/ for hpi files
+PLUGINS_ENDPOINT=http://updates.jenkins-ci.org/latest/
+DATA=/var/lib/jenkins
+
+#mkdir -p $DATA/plugins
+
+#chown jenkins /var/lib/jenkins
+
+if find $DATA/plugins -maxdepth 0 -empty | read v; then
+    (cd $DATA/plugins && wget --no-check-certificate $PLUGINS_ENDPOINT/m2release.hpi)
+fi
+
+#exec su jenkins -c "java -jar /usr/share/jenkins/jenkins.war"
+java -jar /usr/share/jenkins/jenkins.war
